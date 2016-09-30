40038d0ea2d817f5889b7a86ff99f4b0ee95a860
Johan Euphrosine <proppy@google.com>
2016-09-30 16:47:41 -0700

    brillo/drivers: add bmx280 temp sensor driver
    Change-Id: Ifda8817a299b36b7c8bd777f9bc06e97ea7aa550

---
+++ bmx280/src/main/java/com/google/brillo/driver/bmx280/Bmx280.java
@@ -0,0 +1,160 @@
+package com.google.brillo.driver.bmx280;
+
+import android.hardware.pio.I2cDevice;
+import android.hardware.pio.PeripheralManagerService;
+import android.hardware.userdriver.sensors.TemperatureSensorDriver;
+import android.system.ErrnoException;
+import android.util.Log;
+
+import java.io.Closeable;
+import java.util.UUID;
+
+/**
+ * Driver for the BMP/BME 280 temperature sensor.
+ */
+public class Bmx280 implements Closeable {
+    private static final String TAG = Bmx280.class.getSimpleName();
+    /**
+     * I2C address for the the sensor.
+     */
+    public static final int I2C_ADDRESS = 0x77;
+    /**
+     * Sensor constants from the datasheet.
+     * https://cdn-shop.adafruit.com/datasheets/BST-BMP280-DS001-11.pdf
+     */
+    /**
+     * Mininum temperature in Celsius the sensor can measure.
+     */
+    public static final float MIN_TEMP_C = -40.f;
+    /**
+     * Maximum temperature in Celsius the sensor can measure.
+     */
+    public static final float MAX_TEMP_C = 85.f;
+    /**
+     * Maximum power consumption when measure temperature in micro ampere.
+     */
+    public static final float MAX_TEMP_POWER_UA = 325.f;
+    /**
+     * Maximum frequency of the measurements.
+     */
+    public static final float MAX_FREQ_HZ = 181.f;
+    /**
+     * Minimum frequency of the measurements.
+     */
+    public static final float MIN_FREQ_HZ = 23.1f;
+
+    private static final int BMP280_REG_ID = 0xD0;
+    private static final int BMP280_REG_CTRL = 0xF4;
+    private static final int BMP280_REG_TEMP = 0xFA;
+    private static final int BMP280_REG_TEMP_CALIB_1 = 0x88;
+    private static final int BMP280_REG_TEMP_CALIB_2 = 0x8A;
+    private static final int BMP280_REG_TEMP_CALIB_3 = 0x8C;
+    private static final int BMP280_POWER_MODE_NORMAL = 0b00000011;
+    private static final int BMP280_OVERSAMPLING_PRESSURE_SKIP = 0b00000000;
+    private static final int BMP280_OVERSAMPLING_TEMP_1X = 0b00100000;
+
+
+
+    private I2cDevice mDevice;
+    private int mChipId;
+    private int[] mCalibrationData = new int[3];
+
+    /**
+     * Create a new BMP/BME280 sensor driver connected on the given bus.
+     * @param bus I2C bus the sensor is connected to.
+     * @throws ErrnoException
+     */
+    public Bmx280(String bus) throws ErrnoException {
+        PeripheralManagerService pioService = new PeripheralManagerService();
+        I2cDevice device = pioService.openI2cDevice(bus, I2C_ADDRESS);
+        connect(device);
+    }
+    /**
+     * Create a new BMP/BME280 sensor driver connected to the given I2c device.
+     * @param device I2C device of the sensor.
+     * @throws ErrnoException
+     */
+    public Bmx280(I2cDevice device) throws ErrnoException {
+        connect(device);
+    }
+
+    private void connect(I2cDevice device) throws ErrnoException {
+        mDevice = device;
+        mChipId = mDevice.readRegByte(BMP280_REG_ID);
+        // Read unsigned short temp calibration data 1.
+        mCalibrationData[0] = mDevice.readRegWord(BMP280_REG_TEMP_CALIB_1);
+        // Read signed short temp calibration data 2.
+        mCalibrationData[1] = (short) mDevice.readRegWord(BMP280_REG_TEMP_CALIB_2);
+        // Read signed short temp calibration data 3.
+        mCalibrationData[2] = (short) mDevice.readRegWord(BMP280_REG_TEMP_CALIB_3);
+        // Configure Sensor temperature reading.
+        // Power mode: Normal
+        // Temperature resolution: 16bit
+        // Pressure resolution: skip
+        mDevice.writeRegByte(BMP280_REG_CTRL, BMP280_POWER_MODE_NORMAL
+                | BMP280_OVERSAMPLING_TEMP_1X
+                | BMP280_OVERSAMPLING_PRESSURE_SKIP);
+    }
+
+    /**
+     * Close the driver and the underlying device.
+     */
+    @Override
+    public void close() {
+        if (mDevice == null) {
+            throw new IllegalStateException("I2C device is already closed");
+        }
+        mDevice.close();
+        mDevice = null;
+    }
+
+    /**
+     * Returns the sensor chip ID.
+     * @return
+     */
+    public int getChipId() {
+        return mChipId;
+    }
+
+
+    /**
+     * Read a temperate sample (20bit precision).
+     * @return
+     */
+    public float readTemperature() throws IllegalStateException, ErrnoException {
+        if (mDevice == null) {
+            throw new IllegalStateException("I2C device is closed");
+        }
+        byte[] regBytes = mDevice.readRegBuffer(BMP280_REG_TEMP, 3);
+        // msb[7:0] lsb[7:0] xlsb[7:4]
+        int msb = regBytes[0] & 0xff;
+        int lsb = regBytes[1] & 0xff;
+        int xlsb = regBytes[2] & 0xf0;
+        // Convert to 20bit integer
+        int rawTemp = (msb << 16 | lsb << 8 | xlsb) >> 4;
+        // Compensate temperature using calibration data.
+        return compensateTemperature(rawTemp, mCalibrationData);
+    }
+
+    // Compensation formula from the BMP280 datasheet.
+    // https://cdn-shop.adafruit.com/datasheets/BST-BMP280-DS001-11.pdf
+    private static float compensateTemperature(int rawTemp, int[] calibrationData) {
+        // Compensate temperature according to calibration data in the datasheet example.
+        float adc_T = rawTemp;
+        int dig_T1 = calibrationData[0];
+        int dig_T2 = calibrationData[1];
+        int dig_T3 = calibrationData[2];
+        float var1 = (adc_T / 16384.0f - dig_T1 / 1024.0f) * dig_T2;
+        float var2 = ((adc_T / 131072.0f - dig_T1 / 8192.0f) *
+                (adc_T / 131072.0f - dig_T1 / 8192.0f)) * dig_T3;
+        return (var1 + var2) / 5120.0f;
+    }
+
+    /**
+     * Create a new framework temperature sensor driver.
+     * @return the temperature sensor driver.
+     */
+    public TemperatureSensorDriver createTemperatureSensorDriver() {
+        return Bmx280TemperatureSensorDriver.build(this);
+    }
+}
---
+++ bmx280/src/main/java/com/google/brillo/driver/bmx280/Bmx280TemperatureSensorDriver.java
@@ -0,0 +1,39 @@
+package com.google.brillo.driver.bmx280;
+
+import android.hardware.userdriver.sensors.TemperatureSensorDriver;
+import android.system.ErrnoException;
+import android.util.Log;
+
+import java.util.UUID;
+
+class Bmx280TemperatureSensorDriver {
+    private static final String TAG = Bmx280TemperatureSensorDriver.class.getSimpleName();
+    // DRIVER parameters
+    // documented at https://source.android.com/devices/sensors/hal-interface.html#sensor_t
+    private static final String DRIVER_NAME = "BMP280/BME280";
+    private static final String DRIVER_VENDOR = "Bosch";
+    private static final float DRIVER_MAX_RANGE = Bmx280.MAX_TEMP_C - Bmx280.MIN_TEMP_C;
+    private static final float DRIVER_RESOLUTION = DRIVER_MAX_RANGE / 0x100000; // 20bit signed
+    private static final float DRIVER_POWER = Bmx280.MAX_TEMP_POWER_UA / 1000.f;
+    private static final int DRIVER_MIN_DELAY_US = Math.round(1000000.f/Bmx280.MAX_FREQ_HZ);
+    private static final int DRIVER_MAX_DELAY_US = Math.round(1000000.f/Bmx280.MIN_FREQ_HZ);
+    private static final int DRIVER_VERSION = 1;
+    private static final String DRIVER_REQUIRED_PERMISSION = "";
+
+    static TemperatureSensorDriver build(Bmx280 driver) {
+        return new TemperatureSensorDriver(DRIVER_NAME, DRIVER_VENDOR, DRIVER_VERSION,
+                DRIVER_MAX_RANGE, DRIVER_RESOLUTION, DRIVER_POWER,
+                DRIVER_MIN_DELAY_US, DRIVER_REQUIRED_PERMISSION, DRIVER_MAX_DELAY_US,
+                UUID.randomUUID()) {
+            @Override
+            public float read() {
+                try {
+                    return driver.readTemperature();
+                } catch (ErrnoException |IllegalStateException e) {
+                    Log.e(TAG, "error reading bmx280 sensor data:", e);
+                    return Float.NaN;
+                }
+            }
+        };
+    }
+}
