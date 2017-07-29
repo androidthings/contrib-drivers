eda55fd92154b91f3e3d8781edd64bce407835b1
Paul Blundell <blundell.app.dev@gmail.com>
2017-07-29 15:02:15 +0100

    test i2c setup

--- zxsensor/src/test/java/com/google/android/things/contrib/driver/zxsensor/ZxSensorI2cTest.java
+++ zxsensor/src/test/java/com/google/android/things/contrib/driver/zxsensor/ZxSensorI2cTest.java
@@ -1,5 +1,6 @@
 package com.google.android.things.contrib.driver.zxsensor;
 
+import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.GpioDriver;
 import com.google.android.things.pio.I2cDevice;
 
@@ -8,12 +9,14 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Mockito.verify;
+
 public class ZxSensorI2cTest {
 
     @Mock
     private I2cDevice mockDevice;
     @Mock
-    private GpioDriver mockDriver;
+    private GpioDriver mockDataNotify;
 
     @Before
     public void setUp() throws Exception {
@@ -21,8 +24,23 @@ public class ZxSensorI2cTest {
     }
 
     @Test
-    public void name() throws Exception {
-        new ZxSensorI2c("a", "b", mockDevice, mockDriver);
+    public void dataNotifyPinIsActiveHigh() throws Exception {
+        new ZxSensorI2c("i2c", "gpio", mockDevice, mockDataNotify);
 
+        verify(mockDataNotify).setActiveType(Gpio.ACTIVE_HIGH);
+    }
+
+    @Test
+    public void dataNotifyPinIsAnInput() throws Exception {
+        new ZxSensorI2c("i2c", "gpio", mockDevice, mockDataNotify);
+
+        verify(mockDataNotify).setDirection(Gpio.DIRECTION_IN);
+    }
+
+    @Test
+    public void dataNotifyPinTriggersOnTheLeadingEdge() throws Exception {
+        new ZxSensorI2c("i2c", "gpio", mockDevice, mockDataNotify);
+
+        verify(mockDataNotify).setEdgeTriggerType(Gpio.EDGE_RISING);
     }
 }
