1a6e1707c42e203dfacb82fa2d22f1df72c65ce5
Alexander Ehrhardt <der.x@web.de>
2017-08-17 20:37:07 +0200

    Added tests: Close SPI device Close SPI device twice Write red to SPI device

---
+++ ws2812b/src/test/java/com/google/android/things/contrib/driver/ws2812b/ColorMock.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.things.contrib.driver.ws2812b;
+
+import android.graphics.Color;
+
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+
+import static org.mockito.Matchers.anyInt;
+
+class ColorMock {
+    static void mockStatic() {
+        PowerMockito.mockStatic(Color.class);
+
+        mockRed();
+        mockGreen();
+        mockBlue();
+        mockArgb();
+    }
+
+    private static void mockArgb() {
+        Mockito.when(Color.argb(anyInt(), anyInt(), anyInt(), anyInt())).thenAnswer(new Answer() {
+            @Override
+            public Integer answer(InvocationOnMock invocation) throws Throwable {
+                int a = invocation.getArgumentAt(0, Integer.class);
+                int r = invocation.getArgumentAt(1, Integer.class);
+                int g = invocation.getArgumentAt(2, Integer.class);
+                int b = invocation.getArgumentAt(3, Integer.class);
+                return (a << 24) | (r << 16) | (g << 8) | b;
+            }
+        });
+    }
+
+    private static void mockBlue() {
+        Mockito.when(Color.blue(anyInt())).thenAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                int c = invocation.getArgumentAt(0, Integer.class);
+                return c & 0xff;
+            }
+        });
+    }
+
+    private static void mockGreen() {
+        Mockito.when(Color.green(anyInt())).thenAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                int c = invocation.getArgumentAt(0, Integer.class);
+                return (c >> 8) & 0xff;
+            }
+        });
+    }
+
+    private static void mockRed() {
+        Mockito.when(Color.red(anyInt())).thenAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                int c = invocation.getArgumentAt(0, Integer.class);
+                return (c >> 16) & 0xff;
+            }
+        });
+    }
+}
---
+++ ws2812b/src/test/java/com/google/android/things/contrib/driver/ws2812b/Ws2812bTest.java
@@ -0,0 +1,137 @@
+package com.google.android.things.contrib.driver.ws2812b;
+
+
+import android.graphics.Color;
+
+import com.google.android.things.pio.SpiDevice;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(android.graphics.Color.class)
+public class Ws2812bTest {
+    @Mock
+    private SpiDevice mSpiDevice;
+
+    @Rule
+    public MockitoRule mMockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public ExpectedException mExpectedException = ExpectedException.none();
+
+    @Test
+    public void close() throws IOException {
+        Ws2812b leds = new Ws2812b(mSpiDevice, Ws2812b.RGB);
+        leds.close();
+        Mockito.verify(mSpiDevice).close();
+    }
+
+    @Test
+    public void close_safeToCallTwice() throws IOException {
+        Ws2812b ws2812b = new Ws2812b(mSpiDevice, Ws2812b.RBG);
+        ws2812b.close();
+        ws2812b.close();
+        // Check if the inner SPI device was only closed once
+        Mockito.verify(mSpiDevice, Mockito.times(1)).close();
+    }
+
+    @Test
+    public void writeRed() throws IOException {
+        ColorMock.mockStatic();
+
+        int color = Color.RED;
+
+        Ws2812b ws2812b = new Ws2812b(mSpiDevice, Ws2812b.RBG);
+        ws2812b.write(new int [] {Color.RED});
+
+        List<Boolean> bitPatterns = new ArrayList<>();
+        List<Boolean> oneBitPattern = Arrays.asList(true, true, false);
+        List<Boolean> zeroBitPattern = Arrays.asList(true, false, false);
+
+        List<Boolean> originalBits = new ArrayList<>();
+
+        int highestBit = 1<<23;
+
+        for (int i = 0; i < 24; i++) {
+            List<Boolean> bitPattern = (color & highestBit) == highestBit ?  oneBitPattern : zeroBitPattern;
+            bitPatterns.addAll(bitPattern);
+            originalBits.add((color & highestBit) == highestBit);
+            color = color << 1;
+        }
+
+        Iterator<Boolean> iterator = bitPatterns.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            iterator.next();
+            if (i == 8)
+            {
+                iterator.remove();
+                i = 0;
+                continue;
+            }
+            i++;
+        }
+
+
+        int [] masks = {
+                0b1000_0000,
+                0b0100_0000,
+                0b0010_0000,
+                0b0001_0000,
+                0b0000_1000,
+                0b0000_0100,
+                0b0000_0010,
+                0b0000_0001,
+        };
+
+        byte [] bytes = new byte[bitPatterns.size() / 8];
+        byte currentByte = 0;
+        i = 0;
+        int j = 0;
+        for (Boolean bitValue : bitPatterns) {
+            if (bitValue)
+            {
+                currentByte |= masks[i];
+            }
+            if (i == 7)
+            {
+                bytes[j++] = currentByte;
+                currentByte = 0;
+                i = 0;
+                continue;
+            }
+            i++;
+        }
+
+
+
+
+
+
+        Mockito.verify(mSpiDevice).write(bytes, bytes.length);
+    }
+
+    private List<Boolean> flatten(List<List<Boolean>> bitPatterns) {
+        List<Boolean> flattenBitPatterns = new ArrayList<>();
+        for (List<Boolean> bitPattern : bitPatterns) {
+            flattenBitPatterns.addAll(bitPattern);
+        }
+        return flattenBitPatterns;
+    }
+}
