cd4ffdef1416b767f32f6c5b1d3742672c4a009c
Robert Quattlebaum <rquattlebaum@nestlabs.com>
2017-12-05 13:12:40 -0800

    lowpan: Add support for automatic restart after unexpected close
    Bug: b/70231049
    Test: Tested on RPI3 using nRF52840 in USB-CDC-ACM mode.
    Change-Id: I6614c6b64f56517b7d616e9463ecdeff9b9a0bf9

--- lowpan/src/main/java/com/google/android/things/contrib/driver/lowpan/UartLowpanModule.java
+++ lowpan/src/main/java/com/google/android/things/contrib/driver/lowpan/UartLowpanModule.java
@@ -40,15 +40,25 @@ import static com.google.android.things.userdriver.LowpanDriver.ERROR_TOOBIG;
  */
 @SuppressWarnings("WeakerAccess")
 public class UartLowpanModule implements AutoCloseable {
-
+    /** String tag used for logging */
     private static final String TAG = UartLowpanModule.class.getSimpleName();
 
+    /** Listener class to allow UartLowpanModule to signal when it runs into problems. */
+    public interface UnexpectedCloseListener {
+        public void onUnexpectedClose(UartLowpanModule uartLowpanModule);
+    };
+
+    /** Set to {@code true} to enable verbose debug logging */
     private static final boolean DEBUG = false;
+
+    /** Maximum size of a Spinel frame. */
     private static final int MAX_SPINEL_SIZE = 1300;
+
     private final String mUartName;
     private final Handler mHandler;
     private final int mHardwareFlowControl;
     private final int mBaudRate;
+    private final UnexpectedCloseListener mUnexpectedCloseListener;
 
     private UartDevice mDevice;
     private @NonNull LowpanDriverCallback mLowpanDriverCallback;
@@ -58,6 +68,7 @@ public class UartLowpanModule implements AutoCloseable {
     private int mInboundFrameSize;
     private boolean mInboundUnescapeNextByte = false;
     private ByteBuffer mOutputBufferEscaped = ByteBuffer.allocate(MAX_SPINEL_SIZE * 2 + 6);
+    private int mInboundFrameCount = 0;
 
     private final UartDeviceCallback mUartDeviceCallback = new UartDeviceCallback() {
         @Override
@@ -70,6 +81,22 @@ public class UartLowpanModule implements AutoCloseable {
             UartLowpanModule.this.onUartDeviceError(uart, error);
         }
     };
+
+    private static final void debugDump(String desc, byte[] bytes, int count) {
+        if (DEBUG) {
+            final StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < count; i++) {
+                sb.append(String.format("%02X ", bytes[i]));
+            }
+
+            Log.d(TAG, desc + ": " + sb.toString());
+        }
+    }
+
+    private static final void debugDump(String desc, byte[] bytes) {
+        debugDump(desc, bytes, bytes.length);
+    }
 
     /**
      * Create a new UartLowpanModule.
@@ -85,12 +112,15 @@ public class UartLowpanModule implements AutoCloseable {
      * @param handler optional {@link Handler} for UartDevice.
      */
     public UartLowpanModule(@NonNull LowpanDriverCallback lowpanDriverCallback, String uartName,
-                            int baudRate, int hardwareFlowControl, @NonNull Handler handler) throws IOException {
+                            int baudRate, int hardwareFlowControl,
+                            UnexpectedCloseListener unexpectedCloseListener,
+                            @NonNull Handler handler) throws IOException {
         mLowpanDriverCallback = lowpanDriverCallback;
         mUartName = uartName;
         mHardwareFlowControl = hardwareFlowControl;
         mHandler = handler;
         mBaudRate = baudRate;
+        mUnexpectedCloseListener = unexpectedCloseListener;
 
         final PeripheralManagerService manager = new PeripheralManagerService();
 
@@ -107,15 +137,13 @@ public class UartLowpanModule implements AutoCloseable {
      * Close this device and any underlying resources associated with the connection.
      */
     @Override
-    public synchronized void close() {
+    public synchronized void close() throws IOException {
         if (mDevice != null) {
             mDevice.unregisterUartDeviceCallback(mUartDeviceCallback);
             mLowpanDriverCallback = null;
+
             try {
                 mDevice.close();
-            } catch (IOException x) {
-                // If close fails, then we might as well rethrow and crash.
-                throw new RuntimeException(x);
             } finally {
                 mDevice = null;
             }
@@ -133,6 +161,18 @@ public class UartLowpanModule implements AutoCloseable {
         mLowpanDriverCallback.onError(error);
     }
 
+    private void onUnexpectedClose() {
+        try {
+            close();
+        } catch (IOException x) {
+            // We don't really care about IOExceptions here.
+            // If we print out a log message it will likely
+            // become the red herring of many bug reports,
+            // so we just suppress the exception entirely.
+        }
+        mUnexpectedCloseListener.onUnexpectedClose(this);
+    }
+
     /**
      * Sends a frame to the ncp
      *
@@ -145,12 +185,19 @@ public class UartLowpanModule implements AutoCloseable {
             return;
         }
 
-        byte[] outboundFrame = escapeFrame(frame, frame.length);
+        debugDump("sendFrame", frame);
 
+        byte[] outboundFrame = escapeFrame(frame, frame.length);
         int frameSize = outboundFrame.length;
+
         do {
-            int written = mDevice.write(outboundFrame, frameSize);
+            final int written = mDevice.write(outboundFrame, frameSize);
+
+            debugDump("Outbound", outboundFrame, written);
+
             if (frameSize > written) {
+                // We didn't finish writing out everything, so we need to
+                // crop `outboundFrame`, update `frameSize`, and try again.
                 outboundFrame = Arrays.copyOfRange(outboundFrame, written, frameSize);
                 frameSize -= written;
                 continue;
@@ -226,15 +273,17 @@ public class UartLowpanModule implements AutoCloseable {
      */
     private synchronized boolean onUartDeviceDataAvailable(UartDevice uart) {
         try {
-            int count;
-            while ((count = uart.read(mInboundRawBuffer, mInboundRawBuffer.length)) > 0) {
+            final int count = uart.read(mInboundRawBuffer, mInboundRawBuffer.length);
+
+            if (count > 0) {
                 processBuffer(mInboundRawBuffer, count);
             }
+
         } catch (IOException e) {
             Log.w(TAG, "Unable to read UART data", e);
-            onError(ERROR_IOFAIL);
-            close();
+            onUnexpectedClose();
         }
+
         return true;
     }
 
@@ -243,11 +292,12 @@ public class UartLowpanModule implements AutoCloseable {
      */
     private synchronized void onUartDeviceError(UartDevice uart, int error) {
         Log.w(TAG, "Error receiving incoming data: " + error);
-        onError(ERROR_IOFAIL);
-        close();
+        onUnexpectedClose();
     }
 
     private void processBuffer(byte[] buffer, int count) {
+        debugDump("Inbound", buffer, count);
+
         for (int i = 0; i < count; i++) {
             byte current = buffer[i];
 
@@ -267,6 +317,7 @@ public class UartLowpanModule implements AutoCloseable {
 
             if (mInboundFrameSize == MAX_SPINEL_SIZE) {
                 Log.e(TAG, "Inbound frame too large");
+                debugDump("Garbage", mInboundFrame, mInboundFrameSize);
                 onError(ERROR_GARBAGE);
                 mInboundFrameSize++;
 
@@ -294,13 +345,26 @@ public class UartLowpanModule implements AutoCloseable {
             return;
         }
 
-        if (mInboundFrameHDLCCRC != Hdlc.HDLC_CRC_CHECK_VALUE) {
-            Log.e(TAG, String.format("Frame CRC Mismatch: Calc:0x%04X, Expected:0x%04X",
-                    mInboundFrameHDLCCRC, Hdlc.HDLC_CRC_CHECK_VALUE));
-            onError(ERROR_GARBAGE);
-            return;
-        }
+        mInboundFrameCount++;
 
-        mLowpanDriverCallback.onReceiveFrame(Arrays.copyOf(mInboundFrame, mInboundFrameSize-2));
+        if (mInboundFrameHDLCCRC != Hdlc.HDLC_CRC_CHECK_VALUE) {
+            debugDump("Garbage", mInboundFrame, mInboundFrameSize);
+
+            // Skip reporting any initial garbage before the first frame marker.
+            if (mInboundFrameCount > 1) {
+                Log.e(TAG, String.format("Frame CRC Mismatch: Calc:0x%04X, Expected:0x%04X",
+                        mInboundFrameHDLCCRC, Hdlc.HDLC_CRC_CHECK_VALUE));
+
+                onError(ERROR_GARBAGE);
+            }
+
+        } else {
+            // Remove the CRC from the end of the frame.
+            mInboundFrameSize -= 2;
+
+            debugDump("onReceiveFrame", mInboundFrame, mInboundFrameSize);
+
+            mLowpanDriverCallback.onReceiveFrame(Arrays.copyOf(mInboundFrame, mInboundFrameSize));
+        }
     }
 }
