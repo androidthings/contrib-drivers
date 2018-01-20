8f3fd0270ee941415b7b270c3f1de81f9498796a
Blundell <paul@novoda.com>
2018-01-20 20:32:04 +0000

    split out LUTS class to allow formatting

--- inkyphat/src/main/java/com/google/android/things/contrib/driver/inkyphat/InkyPhatV2.java
+++ inkyphat/src/main/java/com/google/android/things/contrib/driver/inkyphat/InkyPhatV2.java
@@ -139,32 +139,7 @@ class InkyPhatV2 implements InkyPhat {
 
         sendCommand(BORDER_CONTROL, new byte[]{border});
 
-        sendCommand(SEND_LUTS, new byte[]{
-        /*Phase 0 Phase 1 Phase 2 Phase 3 Phase 4 Phase 5 Phase 6*/
-        /*A B C D A B C D A B C D A B C D A B C D A B C D A B C D*/
-                0b01001000, (byte) 0b10100000, 0b00010000, 0b00010000, 0b00010011, 0b00000000, 0b00000000,/*0b00000000, #LUT0 - Black*/
-                0b01001000, (byte) 0b10100000, (byte) 0b10000000, 0b00000000, 0b00000011, 0b00000000, 0b00000000,/*0b00000000, #LUTT1 - White*/
-                0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,/*0b00000000, #IGNORE*/
-                0b01001000, (byte) 0b10100101, 0b00000000, (byte) 0b10111011, 0b00000000, 0b00000000, 0b00000000,/*0b00000000, #LUT3 - Red*/
-                0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,/*0b00000000, #LUT4 - VCOM*/
-        /*0xA5, 0x89, 0x10, 0x10, 0x00, 0x00, 0x00, #LUT0 - Black
-        #0xA5, 0x19, 0x80, 0x00, 0x00, 0x00, 0x00, #LUT1 - White
-        #0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, #LUT2 - Red - NADA !
-        #0xA5, 0xA9, 0x9B, 0x9B, 0x00, 0x00, 0x00, #LUT3 - Red
-        #0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, #LUT4 - VCOM*/
-
-/*Duration | Repeat*/
-/*A B C D   |*/
-                67, 10, 31, 10, 4,  /*0 Flash*/
-                16, 8, 4, 4, 6,  /*1 clear*/
-                4, 8, 8, 32, 16,  /*2 bring in the black*/
-                4, 8, 8, 64, 32, /*3 time for red*/
-                6, 6, 6, 2, 2,  /*4 final black sharpen phase*/
-                0, 0, 0, 0, 0,  /*4*/
-                0, 0, 0, 0, 0,  /*5*/
-                0, 0, 0, 0, 0,  /*6*/
-                0, 0, 0, 0, 0   /*7*/
-        });
+        sendCommand(SEND_LUTS, LUTS.data());
 
         sendCommand(RAM_X_ADDRESS, new byte[]{0x00, 0x0c});
         sendCommand(RAM_Y_ADDRESS, new byte[]{0x00, 0x00, (byte) 0xd3, 0x00});
