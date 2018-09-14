71574308facb14f9894291221abfaf69e229731d
Phil Howard <phil@gadgetoid.com>
2018-09-14 11:15:21 +0100

    Added support to specify an alternate i2c address for Cap12xx

--- cap12xx/src/main/java/com/google/android/things/contrib/driver/cap12xx/Cap12xx.java
+++ cap12xx/src/main/java/com/google/android/things/contrib/driver/cap12xx/Cap12xx.java
@@ -192,7 +192,7 @@ public class Cap12xx implements AutoCloseable {
     }
 
     /**
-     * Create a new Cap12xx controller.
+     * Create a new Cap12xx controller with the default I2C address.
      *
      * @param i2cName I2C port name where the controller is attached. Cannot be null.
      * @param alertName optional GPIO pin name connected to the controller's
@@ -214,7 +214,7 @@ public class Cap12xx implements AutoCloseable {
     }
 
     /**
-     * Create a new Cap12xx controller.
+     * Create a new Cap12xx controller with the default I2C address.
      *
      * @param i2cName I2C port name where the controller is attached. Cannot be null.
      * @param alertName optional GPIO pin name connected to the controller's
@@ -223,12 +223,27 @@ public class Cap12xx implements AutoCloseable {
      * @param handler optional {@link Handler} for software polling and callback events.
      * @throws IOException
      */
-    public Cap12xx(String i2cName, String alertName, Configuration chip, Handler handler)
+    public Cap12xx(String i2cName, String alertName, Configuration chip, Handler handler) throws IOException {
+        this(i2cName, I2C_ADDRESS, alertName, chip, handler);
+    }
+
+    /**
+     * Create a new Cap12xx controller.
+     *
+     * @param i2cName I2C port name where the controller is attached. Cannot be null.
+     * @param i2cAddress 7-bit I2C address for the attached controller. Cannot be null.
+     * @param alertName optional GPIO pin name connected to the controller's
+     *                  alert interrupt signal. Can be null.
+     * @param chip identifier for the connected controller device chip.
+     * @param handler optional {@link Handler} for software polling and callback events.
+     * @throws IOException
+     */
+    public Cap12xx(String i2cName, int i2cAddress, String alertName, Configuration chip, Handler handler)
             throws IOException {
         mChipConfiguration = chip;
         try {
             PeripheralManager manager = PeripheralManager.getInstance();
-            I2cDevice device = manager.openI2cDevice(i2cName, I2C_ADDRESS);
+            I2cDevice device = manager.openI2cDevice(i2cName, i2cAddress);
             Gpio alertPin = null;
             if (alertName != null) {
                 alertPin = manager.openGpio(alertName);
