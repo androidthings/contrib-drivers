b9a4bfb5d35950d024bd1c59a55751a020638b1d
Eugene Zadoyannyy <eugene.zadoyannyy@gmail.com>
2019-01-11 21:40:25 +0200

    MCP23017 pin interface was added

---
+++ mcp23017/src/main/java/com/google/android/things/contrib/driver/mcp23017/MCP23017Pin.java
@@ -0,0 +1,10 @@
+package com.google.android.things.contrib.driver.mcp23017;
+
+import com.google.android.things.pio.Gpio;
+
+public interface MCP23017Pin extends Gpio {
+
+    int getAddress();
+
+    int getRegister();
+}
