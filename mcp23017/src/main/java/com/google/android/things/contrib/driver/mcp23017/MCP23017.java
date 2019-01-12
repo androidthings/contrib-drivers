d8117d307f2f872944bbedba7d90a22a61266ba3
Eugene Zadoyannyy <eugene.zadoyannyy@gmail.com>
2019-01-12 10:56:54 +0200

    Copyright was added Refactoring

--- mcp23017/src/main/java/com/google/android/things/contrib/driver/mcp23017/MCP23017.java
+++ mcp23017/src/main/java/com/google/android/things/contrib/driver/mcp23017/MCP23017.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.things.contrib.driver.mcp23017;
 
 import android.util.Log;
@@ -68,6 +84,16 @@ public class MCP23017 {
         return new MCP23017PinImpl(gpio.getName(), gpio.getAddress(), gpio.getRegisters(), this);
     }
 
+    public int getAddress() {
+        return address;
+    }
+
+    public void close() throws IOException {
+        if (device != null) {
+            device.close();
+        }
+    }
+
     void setValue(MCP23017Pin pin, boolean value) throws IOException {
         byte state = device.readRegByte(pin.getRegisters().getGPIO());
         if (value) {
@@ -111,15 +137,5 @@ public class MCP23017 {
             throw new IllegalArgumentException("Unknown active state");
         }
         device.writeRegByte(pin.getRegisters().getIPOL(), activeTypeState);
-    }
-
-    public int getAddress() {
-        return address;
-    }
-
-    public void close() throws IOException {
-        if (device != null) {
-            device.close();
-        }
     }
 }
